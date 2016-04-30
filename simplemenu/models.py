8ad4049ba94b4b74239796d448af375f8dcdf3d3
Richard <richardwangwang@gmail.com>
2016-04-30 11:41:11 +0800

    Code refine.

--- simplemenu/models.py
+++ simplemenu/models.py
@@ -1,5 +1,3 @@
-from django.contrib.contenttypes import generic
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 

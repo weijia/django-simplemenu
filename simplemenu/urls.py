2484ca840c5f81c15fdf7464df7ccf5c0de722d3
Richard <richardwangwang@gmail.com>
2016-06-25 08:50:58 +0800

    Added menu item filter to default url.

--- simplemenu/urls.py
+++ simplemenu/urls.py
@@ -1,9 +1,21 @@
 from django.conf.urls import patterns, include, url
-from djangoautoconf.django_rest_framework_utils.serializer_generator import add_all_urls
+from django.contrib.auth.decorators import login_required
 import models
-
 
 urlpatterns = patterns('',
                        )
+try:
+    from django_auto_filter.views_django_auto_filter_new import DjangoAutoFilterNew
+    urlpatterns += [url(r'^$', login_required(
+        DjangoAutoFilterNew.as_view(
+            model=models.MenuItem,
+        )))]
+except ImportError:
+    pass
 
-add_all_urls(urlpatterns, models)
+
+try:
+    from djangoautoconf.django_rest_framework_utils.serializer_generator import add_all_urls
+    add_all_urls(urlpatterns, models)
+except ImportError:
+    pass

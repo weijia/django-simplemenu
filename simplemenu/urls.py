78980521dda655ddb2e41e3e2e1a1af5b1237fc7
Richard <richardwangwang@gmail.com>
2016-11-20 12:25:42 +0800

    Update codes for new auto filter.

--- simplemenu/urls.py
+++ simplemenu/urls.py
@@ -1,21 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
+from djangoautoconf.model_utils.url_for_models import add_all_urls
+
 import models
 
 urlpatterns = patterns('',
                        )
 try:
-    from django_auto_filter.views_django_auto_filter_new import DjangoAutoFilterNew
+    from django_auto_filter.views_django_auto_filter import DjangoAutoFilter
     urlpatterns += [url(r'^$', login_required(
-        DjangoAutoFilterNew.as_view(
+        DjangoAutoFilter.as_view(
             model=models.MenuItem,
         )))]
 except ImportError:
     pass
 
-
-try:
-    from djangoautoconf.django_rest_framework_utils.serializer_generator import add_all_urls
-    add_all_urls(urlpatterns, models)
-except ImportError:
-    pass
+add_all_urls(urlpatterns, models)
